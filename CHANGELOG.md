5c96cdc99490f033ad2b5765027f729381d2974b
Nate Stuyvesant <nstuyvesant@gmail.com>
2021-10-02 09:57:22 -0400

    chore(forgot): implement first page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,6 @@
 # Backlog
 
 * [High] Implement "Forgot password" functionality in
-  - /src/routes/forgot.svelte
   - /src/routes/auth/forgot.ts endpoint
   - /src/routes/auth/reset/[token].svelte page
   - /src/routes/auth/reset/index.ts endpoint
--- src/routes/forgot.svelte
+++ src/routes/forgot.svelte
@@ -1,28 +1,46 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { goto } from '$app/navigation'
+  import { Toast, ToastBody, ToastHeader } from 'sveltestrap'
 
   export const prerender = true
 
   let focusedField: HTMLInputElement
-
   let email: string
-
   let message: string
+  let isOpen = false
 
   onMount(() => {
     focusedField.focus()
   })
 
-  const sendPasswordReset = () => {
+  const toggle = () => {
+    isOpen = !isOpen
+  }
+
+  const sendPasswordReset = async() => {
     message = ''
     if (email.toLowerCase().includes('gmail.com')) {
       return message = 'GMail passwords must be reset on Manage Your Google Account.'
     }
-
-    // TODO: send email to /auth/forgot
-    // TODO: show Toast
-    goto('/')
+    const url = `/auth/forgot`
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        email
+      }
+    })
+    const result = await res.json()
+    
+    if (res.ok) {
+      toggle()
+      goto('/')
+      return
+    }
+    message = result.message
   }
 </script>
 
@@ -35,10 +53,10 @@
     <div class="card-body">
       <form autocomplete="on" novalidate>
         <h4><strong>Forgot password</strong></h4>
-        <p>It happens to all of us.</p>
+        <p>Hey, we're human.</p>
         <div class="mb-3">
           <label class="form-label" for="email">Email</label>
-          <input type="email" id="email" class="form-control is-large" bind:this={focusedField} bind:value={email} placeholder="Email" autocomplete="email"/>
+          <input bind:this={focusedField} bind:value={email} type="email" id="email" class="form-control is-large" placeholder="Email" autocomplete="email"/>
         </div>
         {#if message}
           <p class="text-danger">{message}</p>
@@ -50,3 +68,8 @@
     </div>
   </div>
 </div>
+
+<Toast class="position-fixed top-0 end-0 m-3" autohide={true} delay={4000} duration={800} {isOpen} on:close={() => (isOpen = false)}>
+  <ToastHeader class="bg-primary text-white" {toggle}>Password Reset</ToastHeader>
+  <ToastBody class="bg-secondary">Please check your inbox for a password reset email.</ToastBody>
+</Toast>
