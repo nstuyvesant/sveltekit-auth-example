38ce5cef7980405babf83a74de320ef316b20111
Nate Stuyvesant <nate@spotlight.ai>
2022-09-05 09:56:12 -0400

    chore(app): release 1.0.18

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,10 +1,10 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = ({ locals }) => {
 	const { user } = locals
 	if (user) { // Redirect to home if user is logged in already
-		throw redirect(302, '/');
+		throw redirect(302, '/')
 	}
 	return {}
 }
