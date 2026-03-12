43e48ede89d0ed87c3207eefdd4b311ae914c75b
Nate Stuyvesant <nstuyvesant@gmail.com>
2026-03-12 16:25:55 -0400

    chore(deps): add

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	build: {
-		sourcemap: process.env.NODE_ENV !== 'production'
-	},
-	plugins: [sveltekit(), tailwindcss()],
+	build: { sourcemap: process.env.NODE_ENV !== 'production' },
+	plugins: [sveltekit(), tailwindcss(), devtoolsJson()],
 	test: {
 		include: ['src/**/*.unit.test.ts', 'tests/**/*.unit.test.ts']
 	},
-	server: {
-		host: 'localhost',
-		port: 3000,
-		open: 'http://localhost:3000'
-	}
-})
+	server: { host: 'localhost', port: 3000, open: 'http://localhost:3000' }
+});
